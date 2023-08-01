07b7f321bfa391ed65cf1b32b1b59942ea3a4ae7
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-01 19:17:10 +0330

    add CommentUpdateView class in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from .models import Post
 
 
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+from .models import Comment
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = "__all__"
+        
+class CommentUpdateView(UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('post_details')
