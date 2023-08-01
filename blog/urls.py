00a77bb9d198a3b805faa0347f75a6808ba34b43
rezatn0934 <rezatn0934@gmail.com>
2023-08-01 19:10:45 +0330

    chore: add update_comment urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, post_list,post_details,category_details,category_list
+from .views import home, post_list,post_details,category_details,category_list, update_comment
 
 urlpatterns = [
     path('', home, name="home"),
@@ -7,6 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/', post_details, name="post_details"),
     path('categories/', category_list, name="category_list"),
     path('categories/<int:pk>/', category_details, name="category_details"),
-
-
+    path('updatecomment/', update_comment, name='update_comment')
 ]
