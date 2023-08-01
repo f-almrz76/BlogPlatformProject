6ce7c36f46dad3d240a88d3fa6159a6edcf1e7f0
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-01 08:22:02 -0700

    form edited

--- blog/forms.py
+++ blog/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class ModelForm(forms.Form):
     title = forms.CharField(max_length=50)
     content = forms.CharField()
-    publication_date = forms.DateField(auto_now_add=True)
+    publication_date = forms.DateField()
     category = forms.CharField()
     author = forms.CharField()
