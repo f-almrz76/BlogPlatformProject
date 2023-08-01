e5bc0237e12181306d51fe1b99f5d4fa1fc78c16
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-01 18:55:51 +0330

    chore: add Comment Update form

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from .models import Post
+from .models import Comment
 
 
 
@@ -9,3 +10,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'category', "author"]
+
+
+class CommentUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
