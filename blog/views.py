01615e58c6386b5d96ca7237a28e771e9980e1db
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-08-01 20:04:39 +0430

    feat:view for post and categories added

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Category, Comment
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Category, Post, Comment
 from users.models import Author
 from django.http import HttpResponse
-
+from .form import CategoryForm, PostForm
 
 # Create your views here.
 
@@ -35,3 +35,51 @@ def category_list(request):
 def category_details(request, pk):
     category = Category.objects.get(id=pk)
     return render(request, "Blog/category_details.html", {"category": category })
+
+def category_list(request):
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+
+    return render(request, 'category_list.html', {'categories': categories, 'form': form})
+
+def category_details(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    posts = Post.objects.filter(category=category)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.category = category
+            post.save()
+            return redirect('category_details', category_id=category_id)
+    else:
+        form = PostForm()
+
+    return render(request, 'category_details.html', {'category': category, 'posts': posts, 'form': form})
+
+
+def post_details(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    # Save the ID of the last seen post in the session
+    request.session['last_post_id'] = post_id
+
+    return render(request, 'post_details.html', {'post': post})
+
+
+def index(request):
+    last_post_id = request.session.get('last_post_id')
+
+    last_post = get_object_or_404(Post, pk=last_post_id) if last_post_id else None
+
+    latest_posts = Post.objects.order_by('-created_at')[:5]
+
+    return render(request, 'index.html', {'last_post': last_post, 'latest_posts': latest_posts})
