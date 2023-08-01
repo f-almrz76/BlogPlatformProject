1084b5ac47848389a00498fd37078b8298df3431
Arad <aradarvinxo@gmail.com>
2023-08-01 19:14:04 +0330

    refactor: undone form added

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
 from .models import Post
+from django.forms import forms
 
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         exclude = ["publication_date", "author"]
+
+
+class UpdateComment(forms.Form):
+    
