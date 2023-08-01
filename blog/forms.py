b7b0d88a7623804f36bf6d634868b40ff11fbb8b
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-01 19:12:19 +0330

    chore: add post create form

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from .models import Post
+from users.models import Author
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'author', 'category']
+
+
+class CommentForm(forms.Form):
+
+    CHOICES = [(author.id, author.name) for author in Author.objects.all()]
+
+    post = forms.IntegerField()
+    author = forms.TypedChoiceField(choices=CHOICES)
+    content = models.TextField()
 
 
