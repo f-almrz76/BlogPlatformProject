429e23fbcea15481f2d98bf92365e77270a04ea5
AnahitaDzl <anahita.dozali@gmail.com>
2023-08-01 19:04:20 +0330

    feat: CommentUpdateForm added

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Post
+from .models import Post,Author
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
 
+class CommentUpdateForm(forms.Form):
+    post = forms.ForeignKey(Post, on_delete=forms.CASCADE)
+    author = forms.ForeignKey(Author, on_delete=forms.CASCADE)
+    content = forms.CharField()
+    comment_date = forms.DateField(auto_now_add=True)
