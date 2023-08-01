71ea0a6ba82edf78b408efa433bd8212c15fc49f
ranadanesh <ranadanesh.79@gmail.com>
2023-08-01 19:09:15 +0430

    Update: comment

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Category, Comment
 from users.models import Author
 from django.http import HttpResponse
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 
@@ -59,3 +59,16 @@ def show_last_post(request):
     last_post_id = request.session.get('last_post_id')
     last_post = Post.objects.get(id=last_post_id)
     return render(request, 'dashboard.html', {'last_post': last_post})
+
+
+def update_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment.content = form.cleaned_data['content']
+            comment.save()
+            return redirect('post_detail', comment_id=comment.id)
+
+    return render(request, 'post_detail.html', {'form': form})
