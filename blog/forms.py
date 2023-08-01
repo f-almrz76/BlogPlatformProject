4df761f0f893b39a2d0a0e54e325c2ac26d85b0d
mabrishami00 <mabrishami78@yahoo.com>
2023-08-01 18:51:51 +0330

    feat: add update comment form

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from django.forms import ModelForm
 from .models import Post
 
 
-class PostForm(forms.Form):
+class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "category", "author"]
+
+
+class UpdateCommentForm(forms.Form):
+    content = forms.TextInput()
