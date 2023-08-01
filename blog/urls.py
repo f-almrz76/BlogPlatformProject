4827eea984ab55db2a909543dfb6688a4f67cee7
ranadanesh <ranadanesh.79@gmail.com>
2023-08-01 19:18:51 +0430

    Update: urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, post_list,post_details,category_details,category_list
+from .views import home, post_list,post_details,category_details,category_list, update_comment
 
 urlpatterns = [
     path('', home, name="home"),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/<int:pk>/', post_details, name="post_details"),
     path('categories/', category_list, name="category_list"),
     path('categories/<int:pk>/', category_details, name="category_details"),
+    path('comments/<int:comment_id>/update/', update_comment, name='update_comment'),
 
 ]
