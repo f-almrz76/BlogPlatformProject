2b8a76f02a7dfead7be2820cddf7a3181d828b08
rezatn0934 <rezatn0934@gmail.com>
2023-08-01 18:35:01 +0330

    chore: define create post form

---
+++ blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Category, Post
+from users.models import Author
+
+
+class CreatePost(forms.Form):
+    title = forms.CharField(max_length=50)
+    content = forms.CharField(widget=forms.Textarea)
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    author = forms.ModelChoiceField(queryset=Author.objects.all())
+
+    def save(self):
+        cd = self.cleaned_data
+        post = Post(title=cd['title'], content=cd['content'], category=cd['category'], author=cd['author'])
+        post.save()
+        return post
