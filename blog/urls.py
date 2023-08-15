2ab5ffa1991937bd76a8ed22dec8f0847391b09d
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-15 18:56:56 +0330

    refactor: update comment_update url to use cbv

--- blog/urls.py
+++ blog/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_details"),
     path("categories/", category_list, name="category_list"),
     path("categories/<int:pk>/", category_details, name="category_details"),
-    path("comment_update/<int:pk>", comment_update, name="comment_update"),
+    path("comment_update/<int:pk>", CommentUpdateView.as_view(), name="comment_update"),
 ]
