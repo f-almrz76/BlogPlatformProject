332b8d8171bc73b80e903a9f2dd1753618d3efb0
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-15 08:46:45 -0700

    feat: admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+
 from .models import Author
 # Register your models here.
 
 admin.site.register(Author)
+author_admin_site = AdminSite(name='author_admin')
