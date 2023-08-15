8b5ee441ea6a8a234da391f99e31b9b35d2c67b8
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-08-15 20:14:40 +0430

    models of users added

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Author
+from django.contrib.admin import AdminSite
+
 # Register your models here.
 
 admin.site.register(Author)
+author_admin_site = AdminSite(name='author_admin')
+
+
