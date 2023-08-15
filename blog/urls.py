1899cda30f0644f41ac3fb785b4b32996d995162
rezatn0934 <rezatn0934@gmail.com>
2023-08-15 19:30:08 +0330

    fix: correct post detail view name

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import home, post_list,PostDetail ,category_details,category_list, CommentUpdate
+from .views import home, post_list,PostDetailView ,category_details,category_list, CommentUpdate
 
 app_name='blog'
 
 urlpatterns = [
     path('', home, name="home"),
     path('post/', post_list, name="post_list"),
-    path('post/<int:pk>/', PostDetail.as_view(), name="post_details"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post_details"),
     path('categories/', category_list, name="category_list"),
     path('categories/<int:pk>/', category_details, name="category_details"),
     path('comment_update/<int:pk>', CommentUpdate.as_view(), name='comment_update'),
