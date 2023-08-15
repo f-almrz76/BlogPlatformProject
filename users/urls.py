855e01592bbe753017c5ef69f6f14ca79b6011a7
mabrishami00 <mabrishami78@yahoo.com>
2023-08-15 19:18:55 +0330

    chore: add authoradmin url

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from .admin import AuthorAdmin
+from .models import Author
+from django.contrib import admin
+
+# admin.site.register(Author, AuthorAdmin)
 
 urlpatterns = [
 
     path('authors/', views.author_list, name="author_list"),
     path('authors/<int:pk>/', views.author_details, name="author_details"),
     path('login/', views.login_view, name='login'),
-
+    path('author_admin/', admin.site.urls)
 ]
