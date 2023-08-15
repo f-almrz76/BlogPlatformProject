cab9c4bf2a37465bcf3f26f023a58e093cff0ee4
mabrishami00 <mabrishami78@yahoo.com>
2023-08-15 19:18:17 +0330

    feat: add authoradmin to  admin panel

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,16 @@ from django.contrib.admin import AdminSite
 
 # Register your models here.
 
-admin.site.register(Author)
+class AuthorAdminSite(AdminSite):
+    site_header = 'Author Administration'
+    site_title = 'Author Admin Site'
+
+author_admin_site = AuthorAdminSite(name='author_admin')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 
 
 
