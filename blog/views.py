543e7ecad6d8a3a61c4e741b40ec47be659d7e17
ranadanesh <ranadanesh.79@gmail.com>
2023-08-15 19:00:44 +0430

    Fix: make the post detail view generic

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,24 @@
+from django.contrib.auth.models import User
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Category, Comment
 from users.models import Author
+from .forms import PostCreationForm, CommentUpdateForm, CommentCreationForm
 from django.http import HttpResponse
-from .forms import PostForm, CommentForm
+from django.views.generic import DetailView, UpdateView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
 
 def home(request):
     context = {}
+    if request.GET.get('search'):
+        search = request.GET['search']
+        cd = Post.objects.filter(Q(title__icontains=search) | Q(content__icontains=search))
+        context = {'searched': cd}
+
     return render(request, 'index.html', context)
 
 
@@ -17,59 +26,73 @@ def post_list(request):
     all_posts = Post.objects.all()
     return render(request, "Blog/post_list.html", {"all_posts": all_posts})
 
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = "Blog/post.html"
 
-def post_details(request, pk):
-    context = {}
-    if request.method == 'GET':
-        post = Post.objects.get(id=pk)
-        context = {"post": post}
-    return render(request, "Blog/post.html", context)
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        post = self.get_object()
+        context["comment"] = post.comment_set.all()
+        return context
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        comment = request.POST.get('comm')
+        author = request.POST.get('username')
+        if comment != None and author != None:
+            if Author.objects.filter(name=author).exists():
+                Comment.objects.create(post=post, author=author, content=comment)
+            else:
+                author = Author.objects.create(name=author)
+                Comment.objects.create(post=post, author=author, content=comment)
+            return redirect('post_details', post.pk)
+
+        return render(request, *args, **kwargs)
+
+
+# def comment_update(request, pk):
+#     comment = Comment.objects.get(id=pk)
+#     if request.method == "POST":
+#         form = CommentUpdateForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             comment.content = cd['content']
+#             comment.save()
+#             return redirect('post_details', comment.post.id)
+#     else:
+#         form = CommentUpdateForm(initial=
+#                                  {'content': comment.content}
+#                                  )
+#
+#     return render(request, 'Blog/comment_update.html', {'form': form, 'comm': comment})
 
 
 def category_list(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+        Category.objects.create(name=name, description=description)
+
     all_category = Category.objects.all()
+
     return render(request, "Blog/category_list.html", {"all_category": all_category})
 
 
 def category_details(request, pk):
-    category = Category.objects.get(id=pk)
-    return render(request, "Blog/category_details.html", {"category": category })
-
-
-def category_view(request):
-    context = {}
-
-    form = PostForm(request.POST or None, request.FILES or None)
-
-    if form.is_valid():
-
-        form.save()
-
-    context['form'] = form
-    return render(request, "category_details.html", context)
-
-
-def view_post(request, post_id):
-    post = Post.objects.get(id=post_id)
-    request.session['last_post_id'] = post_id
-    return render(request, 'post.html', {'post': post})
-
-
-def show_last_post(request):
-    last_post_id = request.session.get('last_post_id')
-    last_post = Post.objects.get(id=last_post_id)
-    return render(request, 'dashboard.html', {'last_post': last_post})
-
-
-def update_comment(request, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
-
     if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment.content = form.cleaned_data['content']
-            comment.save()
-            return redirect('post_detail', comment_id=comment.id)
+        form = PostCreationForm(request.POST)
+        if form.is_valid:
+            f = form.save(commit=False)
+            category = Category.objects.get(id=pk)
+            f.category = category
+            f.save()
 
-    return render(request, 'update_comment.html', {'form': form})
-
+        return redirect('blog:category_details', pk)
+    else:
+        form = PostCreationForm()
+        category = Category.objects.get(id=pk)
+        authors = Author.objects.all()
+        posts = category.post_set.all()
+    return render(request, "Blog/category_details.html",
+                  {"category": category, 'posts': posts, 'authors': authors, 'form': form})
