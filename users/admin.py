909daf938c8312b48656893ca94ad895f86309f6
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-15 18:42:53 +0330

    feat: add Author in custom admin

--- users/admin.py
+++ users/admin.py
@@ -3,8 +3,16 @@ from .models import Author
 from django.contrib.admin import AdminSite
 
 
+
 class AuthorAdminSite(AdminSite):
     site_header = 'Author Admin Panel'
     site_title = 'Author Admin'
 
 author_admin_site = AuthorAdminSite(name='author_admin')
+
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+author_admin_site.register(Author, AuthorAdmin)
