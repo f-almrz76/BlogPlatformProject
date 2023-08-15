d43561bc9548d4be7ed8ada9970deab1c35e9efd
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-15 08:47:02 -0700

    feat: admin url

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from users.admin import author_admin_site
 
 urlpatterns = [
 
     path('authors/', views.author_list, name="author_list"),
     path('authors/<int:pk>/', views.author_details, name="author_details"),
+    path('author_admin/', author_admin_site.urls),
 
 ]
