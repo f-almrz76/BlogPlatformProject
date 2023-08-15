43f05a8fa8833f00626daf5e0a8b085685f043ca
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-15 18:38:29 +0330

    feat: add custom admin site

--- users/admin.py
+++ users/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Author
 from django.contrib.admin import AdminSite
 
-# Register your models here.
 
-admin.site.register(Author)
+class AuthorAdminSite(AdminSite):
+    site_header = 'Author Admin Panel'
+    site_title = 'Author Admin'
 
-
-
+author_admin_site = AuthorAdminSite(name='author_admin')
